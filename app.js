0d9e8c70dfe0caebba5623007224af54c63e497f
Guido Negrisoli <gnegrisoli@gmail.com>
2021-09-19 17:45:14 -0300

    primer commit java

---
+++ app.js
@@ -0,0 +1,15 @@
+let nombre = prompt("Escribe tu nombre")
+console.log(nombre);
+//Guido Negrisoli
+let apellido = prompt("Escribe tu apellido")
+console.log(apellido);
+
+let edad = prompt("Escribe tu edad")
+console.log(edad);
+
+console.log(nombre + " " + apellido + " " + edad);
+let hola = "hola"
+let salida = (`${hola} ${nombre} ${apellido} ${edad}`) //comillas invertidas alt + 96
+
+
+alert(salida)
