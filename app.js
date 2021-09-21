91cf8fb1e1ee83e0224ff98d95cd4c7fd5f511a9
Guido Negrisoli <gnegrisoli@gmail.com>
2021-09-21 20:27:10 -0300

    tp2

--- app.js
+++ app.js
@@ -5,12 +5,39 @@ console.log(nombre);
 let apellido = prompt("Escribe tu apellido")
 console.log(apellido);
 
-let edad = prompt("Escribe tu edad")
+let edad = Number(prompt("Escribe tu edad"))
 console.log(edad);
 
 console.log(nombre + " " + apellido + " " + edad);
+
+
 let hola = "hola"
 let salida = (`${hola} ${nombre} ${apellido} `) //comillas invertidas alt + 96
+let menor = "Sos menor no podes ingresar al sitio"
+let mayor = "Sos mayor podes ingresar "
+let mmayor = "Sos muy mayor para ingresar"
+let admin = "sos admin"
+
+ //alt + 60 <  alt + 62 >
+	if(edad >= 18 && edad <= 49) {
+		console.log("Sos mayor podes ingresar");
+		alert(salida + mayor)
+	}	
+		else if(edad >= 50) {
+			console.log("Sos muy mayor")
+			alert(mmayor)
+		}
+
+		else{
+			console.log("sos menor no podes ingresar");
+			alert(menor)
+
+		}
+	if(nombre === "guido" && apellido === "negrisoli"){
+		console.log("Sos Admin")
+		alert(admin)
+	}
 
 
-alert(salida)
+
+
