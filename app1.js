ada247548dc501bb46b8e9424dcb14ca349f830a
Guido Negrisoli <gnegrisoli@gmail.com>
2021-09-28 21:57:35 -0300

    tp4 2

--- app1.js
+++ app1.js
@@ -1,12 +1,15 @@
 
 
 let producto = Number(prompt("¿cuantos quieres comprar?"))
-let precio1 = 250
+const precio1 = 250
 let resultado = 0
 let resultadoiva= 0
 let iva = 1.21
 let precio10 = 10
 let resultadox10 = 0
+const descuento = 1.10
+
+
 	function calcularPrecio(producto, precio1){	
 	resultado = producto * precio1;
 	}
@@ -30,4 +33,19 @@ mostrar(`el precio sin iva es ${resultado}`);
 	mostrar(`si queres 10 sale ${resultadox10} sin iva `);
 
 	
-
+function calculadora(precio1, producto, operacion) {
+    switch (operacion) {
+        case "*":
+            return precio1 * producto;
+            break;
+      	case "/":
+      		return resultado / descuento;
+      		break;
+        default:
+            return 0;
+            break;
+    }
+}
+console.log(calculadora(precio1, producto, "*"));
+let finaldesc =(calculadora( resultado, descuento, "/" ) ) ;
+mostrar (`En efectivo pagas con descuento ${finaldesc} `)
